9e3596b81a02a9da425e249e8e7156113c1871e1
TransMux <transmux@outlook.com>
2025-02-08 13:39:33 +0800

    ✅ 支持下载用户主页

--- src/yutto/extractor/user_all_ugc_videos.py
+++ src/yutto/extractor/user_all_ugc_videos.py
@@ -9,6 +9,7 @@ from yutto.api.ugc_video import UgcVideoListItem, get_ugc_video_list
 from yutto.exceptions import NotFoundError
 from yutto.extractor._abc import BatchExtractor
 from yutto.extractor.common import extract_ugc_video_data
+from yutto.mux import 检查视频是否下载过, 单个收藏夹最大允许重复次数
 from yutto.utils.asynclib import CoroutineWrapper
 from yutto.utils.console.logger import Badge, Logger
 from yutto.utils.fetcher import Fetcher, FetcherContext
@@ -41,13 +42,30 @@ class UserAllUgcVideosExtractor(BatchExtractor):
         Logger.custom(username, Badge("UP 主投稿视频", fore="black", back="cyan"))
 
         ugc_video_info_list: list[tuple[UgcVideoListItem, str, int]] = []
+
+        repeat = 0
         for avid in await get_user_space_all_videos_avids(ctx, client, self.mid):
             try:
                 ugc_video_list = await get_ugc_video_list(ctx, client, avid)
                 if not Filter.verify_timer(ugc_video_list["pubdate"]):
-                    Logger.debug(f"因为发布时间为 {ugc_video_list['pubdate']}，跳过 {ugc_video_list['title']}")
+                    Logger.debug(
+                        f"因为发布时间为 {ugc_video_list['pubdate']}，跳过 {ugc_video_list['title']}"
+                    )
                     continue
                 await Fetcher.touch_url(ctx, client, avid.to_url())
+
+                if 检查视频是否下载过(str(avid)):
+                    Logger.info(f"已存在 {avid}，跳过")
+                    repeat += 1
+                    if repeat >= 单个收藏夹最大允许重复次数:
+                        Logger.info(
+                            f"重复次数达到 {单个收藏夹最大允许重复次数}，跳过剩余视频"
+                        )
+                        break
+                    continue
+                else:
+                    repeat = max(repeat - 1, 0)
+
                 for ugc_video_item in ugc_video_list["pages"]:
                     ugc_video_info_list.append(
                         (
@@ -73,7 +91,7 @@ class UserAllUgcVideosExtractor(BatchExtractor):
                         "username": username,
                         "pubdate": pubdate,
                     },
-                    "{username}的全部投稿视频/{title}/{name}",
+                    "{owner_uid}-{username}/{bvid}-{name}-{id}",
                 )
             )
             for ugc_video_item, title, pubdate in ugc_video_info_list
